31be4bd2a0708b16dbf135b4fd36ff315e66c526
chrisred8 <chris@hiredbull.com>
2018-09-08 15:31:32 +0800

    swagger

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # sc
 Spring Boot Base Project 
+
+### sc note
+
+* sc 是外面的总的空项目
+* transport 是里面的分项目
+
+
+### Start
+* `TransportApplication` 是总的启动类
+
+
+### Shiro
+`配置参照的 github的项目 后期你可以在基础上增加配置，现在的比较简陋 地址` 
+[shiro项目参照地址](https://github.com/HowieYuan/Shiro-SpringBoot)
+`shiro` 相关的示例类
+
+``` 
+AdminController 
+ExceptionController
+GuestController
+LoginController
+UserController
+
+```
+
+`Postgresql` 集成好了
+
+`Mybatis` 集成好了
+
+* `/sc/transport/src/main/resources/mapper` 是 Mybatis 的配置xml的文件夹
+
+`memcached ` 没集成,用的 `redis` 替代做缓存数据库
+
+
+
+### [Swagger](https://blog.csdn.net/saytime/article/details/74937664)
+
+`Swagger` 相关示例在 `AppController.class` 61行以后, 集成了但是没出来效果
+
+查看地址`http://localhost:8088/swagger-ui.html`
+
+```
+swagger通过注解表明该接口会生成文档，包括接口名、请求方法、参数、返回信息的等等。
+
+@Api：修饰整个类，描述Controller的作用
+@ApiOperation：描述一个类的一个方法，或者说一个接口
+@ApiParam：单个参数描述
+@ApiModel：用对象来接收参数
+@ApiProperty：用对象接收参数时，描述对象的一个字段
+@ApiResponse：HTTP响应其中1个描述
+@ApiResponses：HTTP响应整体描述
+@ApiIgnore：使用该注解忽略这个API
+@ApiError ：发生错误返回的信息
+@ApiImplicitParam：一个请求参数
+@ApiImplicitParams：多个请求参数
+
+```
--- transport/src/main/java/com/dsx/transport/controller/AppController.java
+++ transport/src/main/java/com/dsx/transport/controller/AppController.java
@@ -1,16 +1,20 @@
 package com.dsx.transport.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.dsx.transport.mapper.UserMapper;
 import com.dsx.transport.model.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -54,4 +58,137 @@ public class AppController {
 
         return "success";
     }
+
+    /**
+     * 创建线程安全的Map
+     */
+    static Map<String, User> users = Collections.synchronizedMap(new HashMap<String, User>());
+
+    /**
+     * 根据ID查询用户
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
+    @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> getUserById(@PathVariable(value = "id") Integer id) {
+        JSONObject r = new JSONObject();
+        try {
+            User user = users.get(id);
+            r.put("data", user);
+            r.put("status", "ok");
+        } catch (Exception e) {
+            r.put("data", e.getClass().getName() + ":" + e.getMessage());
+            r.put("status", "error");
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+    /**
+     * 查询用户列表
+     *
+     * @return
+     */
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
+    @RequestMapping(value = "users", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> getUserList() {
+        JSONObject r = new JSONObject();
+        try {
+            List<User> userList = new ArrayList<User>(users.values());
+            r.put("data", userList);
+            r.put("status", "ok");
+        } catch (Exception e) {
+            r.put("data", e.getClass().getName() + ":" + e.getMessage());
+            r.put("status", "error");
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+    /**
+     * 添加用户
+     *
+     * @param user
+     * @return
+     */
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    @RequestMapping(value = "user", method = RequestMethod.POST)
+    public ResponseEntity<JSONObject> add(@RequestBody User user) {
+        JSONObject r = new JSONObject();
+        try {
+            users.put(user.getId(), user);
+            r.put("data", user.getId());
+            r.put("status", "ok");
+        } catch (Exception e) {
+            r.put("data", e.getClass().getName() + ":" + e.getMessage());
+            r.put("status", "error");
+
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+    /**
+     * 根据id删除用户
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除用户")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path")
+    @RequestMapping(value = "user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<JSONObject> delete(@PathVariable(value = "id") Integer id) {
+        JSONObject r = new JSONObject();
+        try {
+            users.remove(id);
+            r.put("data", id);
+            r.put("status", "ok");
+        } catch (Exception e) {
+            r.put("data", e.getClass().getName() + ":" + e.getMessage());
+            r.put("status", "error");
+
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+    /**
+     * 根据id修改用户信息
+     *
+     * @param user
+     * @return
+     */
+    @ApiOperation(value = "更新信息", notes = "根据url的id来指定更新用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long", paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "用户实体user", required = true, dataType = "User")
+    })
+    @RequestMapping(value = "user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<JSONObject> update(@PathVariable("id") Integer id, @RequestBody User user) {
+        JSONObject r = new JSONObject();
+        try {
+            User u = users.get(id);
+            u.setUsername(user.getUsername());
+            users.put("id", u);
+            r.put("data", u);
+            r.put("status", "ok");
+        } catch (Exception e) {
+            r.put("data", e.getClass().getName() + ":" + e.getMessage());
+            r.put("status", "error");
+
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+    @ApiIgnore//使用该注解忽略这个API
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String jsonTest() {
+        return " hi you!";
+    }
+
 }
